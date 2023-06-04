3560f6090e4ae1a3e5096485763a5c0f631a33cb
Sylvester Ezenwata <xthareal@gmail.com>
2023-06-04 22:50:55 +0100

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {    
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.fluttercurve.com',
-                port: '',
-                pathname: '/private/user/**',
-            },
-        ],
-        // domains: ["localhost"]
+        domains: ["api.fluttercurve.com"]
     },
 }
 
