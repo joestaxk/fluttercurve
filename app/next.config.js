7678b17339a85f9cca0ce3f6b48879e430e1f4b8
joestaxk <joestaxk@gmail.com>
2023-06-04 22:08:17 +0100

    cors, and testing production app

--- app/next.config.js
+++ app/next.config.js
@@ -2,22 +2,14 @@
 const nextConfig = {
 
     env: {
-        BASE_URI: 'http://localhost:3000/v1',
-        PUBLIC_PATH: 'http://localhost:3000'
-        // BASE_URI: 'https://api.fluttercurve.com/v1',
-        // PUBLIC_PATH: 'https://api.fluttercurve.com'
+        // BASE_URI: 'http://localhost:3000/v1',
+        // PUBLIC_PATH: 'http://localhost:3000'
+        BASE_URI: 'https://api.fluttercurve.com/v1',
+        PUBLIC_PATH: 'https://api.fluttercurve.com'
     },
     images: {
-        // remotePatterns: [
-        //     {
-        //         protocol: 'https',
-        //         hostname: 'api.fluttercurve.com',
-        //         port: '',
-        //         pathname: '/private/user/**',
-        //     },
-        // ],
-
-        domains: ["localhost"]
+        domains: ['api.fluttercurve.com'], // production 
+        // domains: ["localhost"] // dev
     },
 }
 
