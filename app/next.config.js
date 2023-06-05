7723679434b72ebc4c9040f1594030b5ce9be47b
joestaxk <81616510+joestaxk@users.noreply.github.com>
2023-06-05 08:37:57 +0100

    Update next.config.js
    Rem comment

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BASE_URI: 'http://localhost:3000/v1',
-        PUBLIC_PATH: 'http://localhost:3000'
-        // BASE_URI: 'https://api.fluttercurve.com/v1',
-        // PUBLIC_PATH: 'https://api.fluttercurve.com'
+        //BASE_URI: 'http://localhost:3000/v1',
+       // PUBLIC_PATH: 'http://localhost:3000'
+         BASE_URI: 'https://api.fluttercurve.com/v1',
+        PUBLIC_PATH: 'https://api.fluttercurve.com'
     },
     images: {
         domains: ["api.fluttercurve.com"]
