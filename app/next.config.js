46a55d41ab1ae8f0ff31aa5fc1711303df8313b5
joestaxk <81616510+joestaxk@users.noreply.github.com>
2023-06-05 08:37:15 +0100

    Update next.config.js
    Added back api

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {    
+const nextConfig = {
+    env: {
+        BASE_URI: 'http://localhost:3000/v1',
+        PUBLIC_PATH: 'http://localhost:3000'
+        // BASE_URI: 'https://api.fluttercurve.com/v1',
+        // PUBLIC_PATH: 'https://api.fluttercurve.com'
+    },
     images: {
         domains: ["api.fluttercurve.com"]
     },
