a2f64d53a6dc04c5c31938cea4a14edad4c75517
joestaxk <joestaxk@gmail.com>
2023-06-06 18:35:31 +0100

    Optimize for next export method

--- app/next.config.js
+++ app/next.config.js
@@ -1,26 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {    
     images: {
-        // domains: ["api.fluttercurve.com"]
-        domains: ["localhost"]
+        domains: ["api.fluttercurve.com"]
+        // domains: ["localhost"]
     },
     output: 'export',
     env: {
-        BASE_URI: 'http://localhost:3000/v1',
-       PUBLIC_PATH: 'http://localhost:3000'
-        //  BASE_URI: 'https://api.fluttercurve.com/v1',
-        // PUBLIC_PATH: 'https://api.fluttercurve.com'
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      });
-      return config;
-    },
+      //   BASE_URI: 'http://localhost:3000/v1',
+      //  PUBLIC_PATH: 'http://localhost:3000'
+         BASE_URI: 'https://api.fluttercurve.com/v1',
+        PUBLIC_PATH: 'https://api.fluttercurve.com'
+    }
 }
 
 module.exports = nextConfig
