0af87c8ff03ecf2a09fd638ef54d297fbe2fbe78
joestaxk <joestaxk@gmail.com>
2023-06-10 19:54:47 +0100

    Change base uri and apis too

--- fluttercurve-app/src/lib/requestService.ts
+++ fluttercurve-app/src/lib/requestService.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import helpers from "../helpers";
 
-export const BASE_URI = "http://localhost:3000/v1";
-// export const BASE_URI = "https://api.fluttercurve.com/v1";
-// export const MAIN_URL = "https://fluttercurve.com";
-// export const PUBLIC_PATH = "https://api.fluttercurve.com"
-export const PUBLIC_PATH = "http://localhost:3000"
-export const MAIN_URL = "https://fluttercurve.com"
+// export const BASE_URI = "http://localhost:3000/v1";
+export const BASE_URI = "https://api.fluttercurve.com/v1";
+export const MAIN_URL = "https://fluttercurve.com";
+export const PUBLIC_PATH = "https://api.fluttercurve.com"
+// export const PUBLIC_PATH = "http://localhost:3000"
+// export const MAIN_URL = "https://fluttercurve.com"
 const instance = axios.create({
     baseURL: BASE_URI,
 });
