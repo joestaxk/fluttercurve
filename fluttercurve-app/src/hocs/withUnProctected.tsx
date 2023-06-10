6a40c97278b3024090fea5a6e0aeefcff2d438c7
joestaxk <joestaxk@gmail.com>
2023-06-10 09:11:18 +0100

    resolved error.

--- fluttercurve-app/src/hocs/withUnProctected.tsx
+++ fluttercurve-app/src/hocs/withUnProctected.tsx
@@ -1,19 +1,17 @@
 import { Pageloader } from "../components/utils/buttonSpinner";
 import helpers from "../helpers";
 import { userDataStateType } from "../rState/initialStates";
-import { useRouter } from "next/navigation";
 import {  Suspense, useEffect, } from "react";
 
 export default function withUnProtected(
   WrappedComponent: React.FC<{ state: userDataStateType }>
 ) {
   return function AuthDashboard(props: any) {
-    const router = useRouter()
     const appContextData:userDataStateType = helpers.getLocalItem('user_data') as any
 
     useEffect(() => {
       if(appContextData?.email && helpers.getCookie('xat') && helpers.getCookie('xat')?.split('.').length === 3) {
-        router.push("/office/dashboard")
+        // router.push("/office/dashboard")
       }
     }, [appContextData]);
 
