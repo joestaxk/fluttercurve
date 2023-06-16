54fcc2c440547cbdf0b713cde1c17d190f72fcd6
joestaxk <joestaxk@gmail.com>
2023-06-16 18:53:40 +0100

    Switch route

--- fluttercurve-app/src/lib/requestService.ts
+++ fluttercurve-app/src/lib/requestService.ts
@@ -1,11 +1,19 @@
 import axios from "axios";
 import helpers from "../helpers";
 
-// export const BASE_URI = "http://localhost:3000/v1";
-export const BASE_URI = "https://api.fluttercurve.com/v1";
+
+
+// ENV FILES
+//global
 export const MAIN_URL = "https://fluttercurve.com";
-// export const PUBLIC_PATH = "http://localhost:3000"
 export const MAIN_APP_URL = "https://app.fluttercurve.com";
+
+// prod
+// export const BASE_URI = "https://api.fluttercurve.com/v1";
+
+// dev
+export const BASE_URI = "http://localhost:3000/v1";
+export const PUBLIC_PATH = "http://localhost:3000"
 
 const instance = axios.create({
     baseURL: BASE_URI,
