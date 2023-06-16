0dd27b413baa78a39ec0e8d1c6c90be893677841
joestaxk <joestaxk@gmail.com>
2023-06-16 19:10:41 +0100

    changes on currency

--- fluttercurve-app/src/components/adminDashboard/general/currency.tsx
+++ fluttercurve-app/src/components/adminDashboard/general/currency.tsx
@@ -95,7 +95,7 @@ export default function CurrencyData() {
 
             <div className="bg-orange-200 mt-2 w-full items-center p-6 rounded-lg flex gap-3 text-orange-600">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="orange" d="m10 17.55l-1.77 1.72a2.47 2.47 0 0 1-3.5-3.5l4.54-4.55a2.46 2.46 0 0 1 3.39-.09l.12.1a1 1 0 0 0 1.4-1.43a2.75 2.75 0 0 0-.18-.21a4.46 4.46 0 0 0-6.09.22l-4.6 4.55a4.48 4.48 0 0 0 6.33 6.33L11.37 19A1 1 0 0 0 10 17.55ZM20.69 3.31a4.49 4.49 0 0 0-6.33 0L12.63 5A1 1 0 0 0 14 6.45l1.73-1.72a2.47 2.47 0 0 1 3.5 3.5l-4.54 4.55a2.46 2.46 0 0 1-3.39.09l-.12-.1a1 1 0 0 0-1.4 1.43a2.75 2.75 0 0 0 .23.21a4.47 4.47 0 0 0 6.09-.22l4.55-4.55a4.49 4.49 0 0 0 .04-6.33Z"/></svg>
-                <a href="https://www.iban.com/currency-codes" referrerPolicy="no-referrer" className="">See More currency code.</a>
+                <a href="https://www.iban.com/currency-codes" referrerPolicy="no-referrer" target="_blank">See More currency code.</a>
             </div>
 
 
@@ -151,7 +151,10 @@ export default function CurrencyData() {
                                         )
                                     }
                                 </div>
-                            )) : <></>
+                            )) : <label tabIndex={0} 
+                            className="w-full group border-b-[1px] border-gray-200 flex justify-between items-center p-2">
+                                 Add Something...
+                          </label>
                         }
                     </motion.div>}
                 </div>
